1bf6f7138ac6104b1c3fd53f51f13d8a3c0ea22c
macevoa <macevoa@gmail.com>
2022-07-09 19:39:28 +0400

    fix: fix problems"

--- travel/src/js/index.js
+++ travel/src/js/index.js
@@ -1,17 +1,12 @@
 window.addEventListener('load', pageInit);
 
 console.log(
-  '1. Вёрстка валидная +10\n2. Вёрстка семантическая +20\n   В коде странице присутствуют следующие элементы (указано минимальное количество, может быть больше):\n   - <header>, <main>, <footer> +3\n   - четыре элемента <section> (по количеству секций) +3\n   - только один заголовок <h1> +3\n   - три заголовка <h2> (количество секций минус одна, у которой заголовок <h1>) +3\n   - один элемент <nav> (панель навигации) +3\n   - два списка ul > li > a (панель навигации, ссылки на соцсети) +3\n   - четыре кнопки <button> +2\n3. Вёрстка соответствует макету +48\n   - блок <header> +6\n   - секция preview +9\n   - секция steps +9\n   - секция destinations +9\n   - секция stories +9\n   - блок <footer> +6 \n4. Требования к css + 12\n   - для построения сетки используются флексы или гриды +2\n   - при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2\n   - фоновый цвет тянется на всю ширину страницы +2\n   - иконки добавлены в формате .svg. SVG может быть добавлен любым способом. Обращаем внимание на формат, а не на способ добавления +2\n   - изображения добавлены в формате .jpg +2\n   - есть favicon +2\n5. Интерактивность, реализуемая через css +20\n   - плавная прокрутка по якорям +5\n   - ссылки в футере ведут на гитхаб автора проекта и на страницу курса https://rs.school/js-stage0/ +5\n   - интерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты, например, изменение цвета фона или цвета шрифта. Если в макете указаны стили при наведении и клике, для элемента указываем эти стили. Если в макете стили не указаны, реализуете их по своему усмотрению, руководствуясь общим стилем макета +5\n   - обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +5\nTotal: 110 / 110 pts.',
+  '1. Вёрстка соответствует макету. Ширина экрана 390px +48\n   - блок `<header>` +6\n   - секция `preview` +9\n   - секция `steps` +9\n   - секция `destinations` +9\n   - секция `stories` +9\n   - блок `<footer>` +6\n2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n   - нет полосы прокрутки при ширине страницы от 1440рх до 390px +7\n   - нет полосы прокрутки при ширине страницы от 390px до 320рх +8\n3. На ширине экрана 390рх и меньше реализовано адаптивное меню +22\n   - при ширине страницы 390рх панель навигации скрывается, появляется бургер-иконка +2\n   - при нажатии на бургер-иконку плавно появляется адаптивное меню +4\n   - адаптивное меню соответствует макету +4\n   - при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\n   - ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4 (все кроме Account, она пока что просто закрывает меню)\n   - при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4\n',
 );
 
 function pageInit() {
-  setPageHeightStyle();
   createBurgerClickListener();
   createNavClickListener();
-}
-
-function setPageHeightStyle() {
-  document.documentElement.style.height = `${calcPageHeight() - 251}px`;
 }
 
 function createBurgerClickListener() {
@@ -19,8 +14,8 @@ function createBurgerClickListener() {
 }
 
 function showMobileNavigation() {
-  const nav = document.querySelector('.nav');
-  nav.style.right = '0px';
+  const nav = document.querySelector('.burger-nav');
+  nav.style.width = '165px';
   createBackgroundLayer();
   scrollToTop();
   blockPageScrolling();
@@ -28,27 +23,27 @@ function showMobileNavigation() {
 }
 
 function createNavCloseEventListener() {
-  document.querySelector('.nav__close').addEventListener('click', closeMobileNavigation);
+  document.querySelector('.burger-nav__close').addEventListener('click', closeMobileNavigation);
 }
 
 function removeNavCloseEventListener() {
-  document.querySelector('.nav__close').removeEventListener('click', closeMobileNavigation);
+  document.querySelector('.burger-nav__close').removeEventListener('click', closeMobileNavigation);
 }
 
 function createNavClickListener() {
-  const nav = document.querySelector('.nav');
+  const nav = document.querySelector('.burger-nav');
   nav.addEventListener('click', navClickHandler);
 }
 
 function navClickHandler(ev) {
-  if (Array.from(ev.target.classList).includes('nav__link')) {
+  if (Array.from(ev.target.classList).includes('burger-nav__link')) {
     closeMobileNavigation();
   }
 }
 
 function closeMobileNavigation(ev) {
-  const nav = document.querySelector('.nav');
-  nav.style.right = '-165px';
+  const nav = document.querySelector('.burger-nav');
+  nav.style.width = '0px';
   removeNavCloseEventListener();
   removeBackgroundLayer();
   unblockPageScrolling();
